add0dd342f912b88dde99d39c5f96c4dd1aedf67
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-24 20:41:37 -0300

    Adicionar Markup para os produtos

--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -1,4 +1,4 @@
-import { useEffect, useContext, useState } from "react";
+import { useEffect, useContext, useState, Fragment } from "react";
 import { useRouter } from "next/router";
 
 import { FirebaseContext } from "@/firebase";
@@ -6,6 +6,17 @@ import { collection, getDoc, doc } from "firebase/firestore";
 
 import Layout from "../../components/layout/Layout";
 import Error404 from "@/components/layout/404";
+
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
+
+const ContenedorProducto = styled.div`
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    column-gap: 2rem;
+  }
+`;
 
 const Producto = () => {
   // state del componente
@@ -22,14 +33,18 @@ const Producto = () => {
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
+    console.log(id);
     if (id) {
       const obtenerProducto = async () => {
-        const productoQuery = doc(collection(firebase.db, "productos"), id);
+        const productoQuery = await doc(
+          collection(firebase.db, "productos"),
+          id
+        );
 
-        const productoID = getDoc(productoQuery);
+        const producto = await getDoc(productoQuery);
 
-        if (productoID.exists) {
-          setProducto(productoID.data());
+        if (producto.exists()) {
+          setProducto(producto.data());
         } else {
           setError(true);
         }
@@ -38,7 +53,41 @@ const Producto = () => {
     }
   }, [id]);
 
-  return <Layout>{error && <Error404 />}</Layout>;
+  //   if (Object.keys(producto).length === 0) return "Cargando...";
+
+  const {
+    comentarios,
+    creado,
+    descripcion,
+    empresa,
+    nombre,
+    url,
+    URLImage: urlimagen,
+    votos,
+  } = producto;
+
+  return (
+    <Layout>
+      <Fragment>
+        {error && <Error404 />}
+        <div className="contenedor">
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            {nombre}
+          </h1>
+
+          <ContenedorProducto>
+            <div>1</div>
+            <aside>2</aside>
+          </ContenedorProducto>
+        </div>
+      </Fragment>
+    </Layout>
+  );
 };
 
 export default Producto;
