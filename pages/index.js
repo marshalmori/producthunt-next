f26d0d7e51171ddda6b595fdc4e8074f61e8939e
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-24 14:09:14 -0300

    Consultar o banco de datos para trazer os produtos

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,27 @@
+import { useEffect, useState, useContext } from "react";
 import Layout from "@/components/layout/Layout";
+import { FirebaseContext } from "@/firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 export default function Home() {
+  const [productos, guardarProductos] = useState([]);
+
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    const obtenerProductos = async () => {
+      const querySnapshot = await getDocs(collection(firebase.db, "productos"));
+      const productos = querySnapshot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      guardarProductos(productos);
+    };
+    obtenerProductos();
+  }, []);
+
   return (
     <div>
       <Layout>
